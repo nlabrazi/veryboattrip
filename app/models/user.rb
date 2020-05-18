9f72449abb5d45a06ee536da66a1148138e9e4c4
Nabil Labrazi <na.labrazi@gmail.com>
2020-05-18 16:20:41 +0200

    ajout des relations users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :boats
+  has_many :deals
+  has_many :reviews
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
 end
