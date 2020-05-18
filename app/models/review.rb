7a1d16c848423b1fd131574adb7675c4492c975f
Nabil Labrazi <na.labrazi@gmail.com>
2020-05-18 16:10:13 +0200

    ajout des relations to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ApplicationRecord
+  belongs_to :deals
+  belongs_to :users
+
+  validates :rating, presence: true
+  validates :title, presence: true
+  validates :comment, presence: true
 end
