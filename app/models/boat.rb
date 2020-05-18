153a9b45dacb6f1a6a0a0dd5c2097fd5752f385d
Nabil Labrazi <na.labrazi@gmail.com>
2020-05-18 15:36:02 +0200

    ajout des relations pour boat

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,2 +1,8 @@
 class Boat < ApplicationRecord
+  has_many :deals
+  has_many :reviews
+
+  validates :name, uniqueness: true, presence: true
+  validates :type, presence: true
+  validates :size, presence: true
 end
