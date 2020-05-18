6d7dee56a0454839f9c88704e57eca45bd81091d
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-05-18 15:04:24 +0200

    Creation BoatsController avec method

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,33 @@
+class BoatsController < ApplicationController
+  before_action :set_boat
+
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to user_boat_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_boat
+    @boat = Boat.find(params[:id])
+  end
+
+  # def boat_params
+  #   params.require(:boat).permit(, :photo)
+  # end
+end
