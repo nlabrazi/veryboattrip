aeaa1e4384a5d545bea55fefe70d14673c33ca3f
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-05-19 15:51:17 +0200

    landing backgroun fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :landing]
+
+  def landing
+    @user = current_user
+  end
 
   def home
     @boats = Boat.all
     @user = current_user
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
+  get "/home", to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, except: :destroy do
